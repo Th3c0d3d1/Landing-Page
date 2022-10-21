3b8c0266284cb2e8d022cc852f9a7caaea88c201
Th3c0d3d1 <bittradeguy007@gmail.com>
2022-10-21 23:30:27 +0000

    start of compilation

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,7 +3,10 @@ import React from "react";
 //include images into your bundle
 // import rigoImage from "../../img/rigo-baby.jpg";
 import Jumbotron from "./Jumbotron.jsx";
-import 
+import card from "./card.jsx";
+import NavBar from "./NavBar.jsx";
+import FooterBottom from "./FooterBottom.jsx";
+
 //create your first component
 const Home = () => {
 	return (
