7fe08e6eb4af5d39f0f9533c038c79ebcb0c6eaf
Th3c0d3d1 <bittradeguy007@gmail.com>
2022-10-26 00:23:33 +0000

    compilation

--- src/js/component/FooterBottom.jsx
+++ src/js/component/FooterBottom.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+// import { Footer } from 'react-bootstrap';
+
 
 const FooterBottom = (props) => {
   let name = props.name;
   return (
+    
 <body className="d-flex flex-column">
+<Footer/>
   <footer id="sticky-footer" className="flex-shrink-0 py-4 bg-dark text-white-50 fixed-bottom">
     <div className="container text-center">
       <small>Copyright &copy; Your Website</small>
--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = (props) => {
   let name = props.name;
   return (
     // <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark navbar-dark">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light na">
             <div className="container-fluid">
             <a className="navbar-brand w-80" href="#">Start Bootstrap</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
