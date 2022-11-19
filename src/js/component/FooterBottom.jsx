c76af15540f1b987b73f014bb609ec695a288279
Th3c0d3d1 <bittradeguy007@gmail.com>
2022-11-19 02:04:42 +0000

    last time

--- src/js/component/FooterBottom.jsx
+++ src/js/component/FooterBottom.jsx
@@ -3,11 +3,11 @@ import React from "react";
 
 
 const FooterBottom = (props) => {
-  let name = props.name;
+  // let name = props.name;
   return (
     
 <body className="d-flex flex-column">
-<Footer/>
+{/* <Footer/> */}
   <footer id="sticky-footer" className="flex-shrink-0 py-4 bg-dark text-white-50 fixed-bottom">
     <div className="container text-center">
       <small>Copyright &copy; Your Website</small>
